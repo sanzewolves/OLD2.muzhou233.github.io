f81abb0cb583fc336fc7023b3c5a57833df949d3
MuZhou233 <MuZhou233@outlook.com>
2019-06-23 00:34:48 +0800

    fix

--- js/muzhou-blog.js
+++ js/muzhou-blog.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var jumpurl = $(this).attr('href');
     if (typeof jumpurl != 'undefined' && jumpurl[0] != '#') {
       jumpevent.preventDefault();
-      var reg = RegExp(/^https:\/\/muzhou.tech.+/);
+      var reg = RegExp(/^https://muzhou.tech/);
       if(jumpurl.match(reg)){
         jumpevent.preventDefault();
         $('#loading').fadeIn('fast');
@@ -20,7 +20,7 @@ $(document).ready(function () {
     var jumpurl = $(this).attr('href');
     if (typeof jumpurl != 'undefined' && jumpurl[0] != '#') {
       jumpevent.preventDefault();
-      var reg = RegExp(/^https:\/\/muzhou.tech.+/);
+      var reg = RegExp(/^https://muzhou.tech/);
       if(jumpurl.match(reg)){
         jumpevent.preventDefault();
         $('#loading').fadeIn('fast');
